9427c6bc4d40041ed94087d0446a5f157cf90338
jasons1142 <jasons11@bu.edu>
2025-06-15 15:12:38 -0400

    Added navbar to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Navbar
+ from "@/components/Navbar"
 export default function Home() {
   return (
     <main className = "bg-stone-200 min-h-screen">
-      <div className = "text-center mb-30 bg-gray-400 w-full py-6">
+      <div className = "text-center bg-gray-400 w-full py-6">
         <h1 className="text-5xl font-bold uppercase text-black">BlackBalannce</h1>
       </div>
+      <Navbar/>
       <div className = "bg-gray-600 p-15 rounded-lg text-white w-full max-w-xl mx-auto">
         <p>Welcome to BlackBalannce a webpage designed to focus on the mental needs of black people.
           On this site you will find mental health resources, a personal notebook, and a friendly forum to communicate with others.
---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-400 py-4 mb-30">
+      <div className=" flex justify-center space-x-6 w-full">
+        <Link href="/">Home</Link>
+        <Link href="/resources">Resources</Link>
+        <Link href="/journal">Journal</Link>
+        <Link href="/forum">Forum</Link>
+      </div>
+    </nav>
+  );
+}
