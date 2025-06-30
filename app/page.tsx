15c7548ea1b263c42535651e717b6bb17bfffdf6
jasons1142 <jasons11@bu.edu>
2025-06-30 14:01:36 -0400

    Added footer to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
-import Navbar
- from "@/components/Navbar"
-export default function Home() {
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+export default function HomePage() {
   return (
-    <main className = "bg-stone-200 min-h-screen">
-      <div className = "text-center bg-gray-400 w-full py-6">
-        <h1 className="text-5xl font-bold uppercase text-black">BlackBalannce</h1>
-      </div>
-      <Navbar/>
-      <div className = "bg-gray-600 p-15 rounded-lg text-white w-full max-w-xl mx-auto">
-        <p>Welcome to BlackBalannce a webpage designed to focus on the mental needs of black people.
-          On this site you will find mental health resources, a personal notebook, and a friendly forum to communicate with others.
-        </p>
-      </div>
-    </main>
+    <div>
+      <main className = "bg-stone-200 min-h-screen">
+        <div className = "text-center bg-gray-400 w-full py-6">
+          <h1 className="text-5xl font-bold uppercase text-black">BlackBalannce</h1>
+        </div>
+        <Navbar/>
+        <div className = "bg-gray-600 p-15 rounded-lg text-white w-full max-w-xl mx-auto">
+          <p>Welcome to BlackBalannce a webpage designed to focus on the mental needs of black people.
+            On this site you will find mental health resources, a personal notebook, and a friendly forum to communicate with others.
+          </p>
+        </div>
+      </main>
+      <Footer/>
+    </div>
+    
+    
   )
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white text-center py-4">
+            <p>&copy; {new Date().getFullYear()} BlackBalannce. All rights reserved.</p>
+        </footer>
+    )
+}
