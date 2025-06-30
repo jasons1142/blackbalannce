878ff05feb53e2bfc034a8d26f4942ca27841be2
jasons1142 <jasons11@bu.edu>
2025-06-30 15:17:51 -0400

    Began resources page

---
+++ app/resources/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+export default function Resources() {
+    <div>
+        <main className = "bg-stone-200 min-h-screen">
+            <Navbar/>
+        </main>
+        <Footer/> 
+    </div>
+}
