a37f34a1244afd7687d79f46315b719329575b5a
jasons1142 <jasons11@bu.edu>
2025-07-01 10:22:37 -0400

    Updates to the resources

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -7,7 +7,19 @@ export default function Resources() {
             <main className = "bg-stone-200 min-h-screen">
                 <Header />
                 <Navbar/>
-                <h1 className = "text-center text-black">Below are resources that can be helpful for your mental health journey</h1>
+                <h1 className = "text-center text-black font-bold text-xl mt-5">Below are resources that can be helpful on your mental health journey</h1>
+                <label className = "text-black ml-2 font-bold">Mental Health & Wellness</label>
+                <ul className = "text-black ml-5">
+                    <li></li>
+                </ul>
+
+                <label className = "text-black ml-2 font-bold">Identity & Empowerment</label>
+                <ul className = "text-black ml-5">
+                    <li></li>
+                </ul>
+
+                <label className = "text-black ml-2 font-bold">Self-Care & Tools</label>
+
             </main>
             <Footer/> 
         </div>
